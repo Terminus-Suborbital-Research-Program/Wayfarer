86dcd42a9de43f4541798a463f94f54567754d67
supergoodname77 <epascuales@outlook.com>
2026-02-13 17:42:36 -0600

    Statistical bench and lib crate

--- benches/tracker_bench.rs
+++ benches/tracker_bench.rs
@@ -36,6 +36,8 @@ use divan::{Bencher, black_box};
         serde::{decode_from_std_read, encode_into_std_write},
     };
 
+    use rand::Rng;
+    use rand::RngExt;
 
 
 fn main() {
@@ -49,34 +51,8 @@ fn tracker_bench(bencher: Bencher) {
     let starfinder = Starfinder::default();
     let camera_model = CameraModel::default();
     let startracker = Startracker::default();
-    
-    let images: Vec<_> = fs::read_dir("../images/mixed")
-        .unwrap()
-        .filter_map(|e| e.ok())
-        .map(|e| {
-            let img = image::open(e.path()).unwrap();
-            image::GrayImage::from(img)
-        })
-        .collect();
 
-    bencher.bench_local(move || {
-        for mut gray_image in images.clone() {
-            let mut centroids = starfinder.star_find(&mut gray_image);
-            camera_model.undistort_centroids(&mut centroids);
-            let _ = black_box(startracker.pyramid_solve(centroids));
-        }
-    });
-}
-
-#[divan::bench]
-fn tracker_bench_single_frame(bencher: Bencher) {
-    // 1. Global Setup (Run once)
-    let starfinder = Starfinder::default();
-    let camera_model = CameraModel::default();
-    let startracker = Startracker::default();
-    
-    // Load ALL images, but we will just pick the first one to benchmark
-    // This represents a "typical" frame.
+    // Load all images and select a random one, repeat bench against it
     let images: Vec<GrayImage> = fs::read_dir("../images/mixed")
         .unwrap()
         .filter_map(|e| e.ok())
@@ -86,23 +62,19 @@ fn tracker_bench_single_frame(bencher: Bencher) {
         })
         .collect();
 
-    let sample_image = images.first().expect("No images in folder!").clone();
+    let mut rng = rand::rng();
+    let len = images.len();
+    let idx = rng.random_range(0..len);
+
+    let sample_image = images[idx].clone();
 
     bencher
-        // 2. Per-Iteration Setup (NOT TIMED)
-        // We clone the image here so the cost of "copying" is excluded from the result.
-        // We need a fresh image every time because star_find modifies it.
         .with_inputs(|| sample_image.clone()) 
-        
-        // 3. The Measurement (TIMED)
         .bench_values(|mut img| {
-            // A. Find
             let mut centroids = starfinder.star_find(&mut img);
             
-            // B. Undistort
             camera_model.undistort_centroids(&mut centroids);
             
-            // C. Solve
             // Wrap in black_box so the compiler doesn't optimize it away
             black_box(startracker.pyramid_solve(centroids))
         });
